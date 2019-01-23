a29eab36421227327b1cfbd7a20bbae4372ee29d
Brian <trandangquyen@gmail.com>
2019-01-23 18:24:45 +0700

    update LPO

--- js/homepage.js
+++ js/homepage.js
@@ -6,6 +6,41 @@
             controlNav: "thumbnails"
           });
     }
+    function runCarousel() {
+        $('.carousel').carousel({
+            hAlign: 'left',
+            hMargin: 0.3,
+            slidesPerScroll:3,
+            carouselWidth: 550,
+            carouselHeight: 405,
+            left: 0,
+            directionNav: false,
+            shadow: false,
+            buttonNav: 'none'
+        });
+    }
+    function runSlickSlider() {
+        $(".tutor-slider,.std-slider,.offer-slider").slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            centerMode: true,
+            centerPadding: '80px',
+            focusOnSelect: true
+        });
+    }
+    function addClassActiveTab() {
+        $('.elite-tabs li').click(function(event) {
+            if($(this).index() == 1) {
+                $(this).siblings().addClass('no-split')
+            }
+            else {
+                 $(this).removeClass('no-split');
+                 $(this).siblings().removeClass('no-split');
+            }
+        });
+    }
     /* ----------------------------------------------- */
     /* ------------- FrontEnd Functions -------------- */
     /* ----------------------------------------------- */
@@ -13,12 +48,8 @@
     /* OnLoad Page */
     $(document).ready(function($) {
         // runSlider();
-
+        runCarousel();
+        runSlickSlider();
+        addClassActiveTab();
     });
-
-    /* OnLoad Window */
-    var init = function() {
-
-    };
-    window.onload = init;
 })(jQuery);
